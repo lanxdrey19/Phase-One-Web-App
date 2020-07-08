5df8be20658d4d7c08297d8598c2695b6a78a4b9
Lance Delos Reyes <lan_drey19@hotmail.com>
2020-07-08 17:16:51 +1200

    Displays a Search Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import SearchBar from './Components/SearchBarComponent/SearchBar';
 
 function App() {
   return (
     <div>
+      <SearchBar/>
     </div>
   );
 }
